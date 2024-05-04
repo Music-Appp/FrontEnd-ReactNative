b83f4fac4f6aed20edb6238f20de2cb1e2925628
feedingpet <133138693+feedingpet@users.noreply.github.com>
2024-05-05 01:35:29 +0700

    Add files via upload
    create welcome screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,58 @@
 import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { ScrollView, Image, Button, TextInput, ImageBackground } from 'react-native';
 import { useEffect, useState } from 'react';
 import Album from '@/components/ALBUM';
 
-export default function TabOneScreen() {
-  const [album, setAlbum] = useState<string[] | undefined>();
-  const [song, setSong] = useState<string[] | undefined>();
-  const [singer, setSinger] = useState<string[] | undefined>();
+export const BACKGROUND_IMAGE_URL = '../assets/images/background.jpg'
+export const LOGO_IMAGE_URL = '../assets/images/logo.jpg'
 
-  useEffect(() => {
-    setAlbum(['YEU 5', 'DIEU TUYET VOI', 'MOI NGAY MOT NIEM VUI', 'CHUNG TA LA CUA NHAU', 'YEU 5', 'DIEU TUYET VOI', 'MOI NGAY MOT NIEM VUI', 'CHUNG TA LA CUA NHAU', 'YEU 5', 'DIEU TUYET VOI', 'MOI NGAY MOT NIEM VUI', 'CHUNG TA LA CUA NHAU']);
-    setSong(['ONG BA ANH', 'MOT NHA', 'THANH XUAN', 'BAI CA TUOI TRE']);
-    setSinger(['SON TUNG MTP', 'BICH PHUONG', 'MCK', 'MR.SIRO']);
-  }, []);
 
+export default function HomeScreen () {
   return (
-    <View style={styles.container}>
-      <View style={styles.gridContainer}>
-        <Text style={styles.title}>Album</Text>
-        <Album listAlbum={album} />
-        <Text style={styles.title}>Song</Text>
-        <Album listAlbum={song} />
-        <Text style={styles.title}>Singer</Text>
-        <Album listAlbum={singer} />
-      </View>
+    <View style={ styles.container }>
+      <ImageBackground 
+        source={{
+          uri: BACKGROUND_IMAGE_URL,
+        }}
+        style={styles.container}
+      >
+      <Text style={styles.titleText}>Welcome to ours app!</Text>
+      <Image
+          source={{
+            uri: LOGO_IMAGE_URL,
+          }}
+          style={styles.image}
+        />
+      </ImageBackground>
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  title: {
-    fontSize: 20,
+  titleText: {
+    fontSize: 35,
+    color: '#f457ef',
+    textAlign: 'center',
+    fontFamily: 'sans-serif',
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  gridContainer: {
 
+  image: {
+    width: 100,
+    height: 100,
+    flex: 1,
+    position:'absolute',
+    bottom: 0,
   },
+
+  button: {
+    alignItems: "center",
+  }
+
 });
